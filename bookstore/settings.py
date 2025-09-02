06cc8eea2965be5623b7fbef051f73e573be89c5
setayeshbaghaee <148934538+setayeshbaghaee@users.noreply.github.com>
2025-09-02 20:00:15 +0330

    Update settings.py

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -72,10 +72,11 @@ ROOT_URLCONF = 'bookstore.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templates"], 
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
+                'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
@@ -83,6 +84,7 @@ TEMPLATES = [
         },
     },
 ]
+
 
 WSGI_APPLICATION = 'bookstore.wsgi.application'
 
